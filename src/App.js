f73b66eb88a3486648edea84088bb1e92b1f3fee
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-08-11 13:50:12 +0100

    navbar component + linsk sorted

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Explore from './pages/Explore';
+import ForgotPassword from './pages/ForgotPassword';
+import Navbar from './components/Navbar';
+import Offers from './pages/Offers';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
 function App() {
   return (
     <>
-      <h1>My App</h1>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Explore />}></Route>
+          <Route path='/forgot-password' element={<ForgotPassword />}></Route>
+          <Route path='/offers' element={<Offers />}></Route>
+          <Route path='/profile' element={<SignIn />}></Route>
+          <Route path='/sign-in' element={<SignIn />}></Route>
+          <Route path='/sign-up' element={<SignUp />}></Route>
+        </Routes>
+        <Navbar />
+      </Router>
     </>
   );
 }
