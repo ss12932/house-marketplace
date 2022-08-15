9b2e6acf9493ec698c41e8be614325f73fa364ef
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-08-15 14:05:55 +0100

    priv route comp made + useAuthStatus cust hook

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import Explore from './pages/Explore';
 import ForgotPassword from './pages/ForgotPassword';
 import Navbar from './components/Navbar';
+import PrivateRoute from './components/PrivateRoute';
 import Offers from './pages/Offers';
 import Profile from './pages/Profile';
 import SignIn from './pages/SignIn';
@@ -17,7 +18,9 @@ function App() {
           <Route path='/' element={<Explore />}></Route>
           <Route path='/forgot-password' element={<ForgotPassword />}></Route>
           <Route path='/offers' element={<Offers />}></Route>
-          <Route path='/profile' element={<Profile />}></Route>
+          <Route path='/profile' element={<PrivateRoute />}>
+            <Route path='/profile' element={<Profile />}></Route>
+          </Route>
           <Route path='/sign-in' element={<SignIn />}></Route>
           <Route path='/sign-up' element={<SignUp />}></Route>
         </Routes>
